2311632aeb2b4ea15e7b5feaf5283ba89fd1f13f
macos <macos@macoss-iMac.local>
2022-02-22 10:05:44 +0700

    First commit

---
+++ App.js
@@ -0,0 +1,243 @@
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Image, View, Text, TouchableOpacity, Linking } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import detailScreen from './loginDetail';
+
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='LoginScr'>
+        <Stack.Screen name='LoginScr' component={LoginScreen} options={{title:'Log In'}}/>
+        <Stack.Screen name='RegisterScr' component={RegisterScreen} options={{title:'Register'}}/>
+        <Stack.Screen name='LoginDetailScr' component={detailScreen}  options={{title:'Detail'}, {headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+/////////////////////
+///Register Screen///
+/////////////////////
+
+const storage = async (user, pass) => {
+  try {
+    const JsonObject = JSON.stringify({userName: user, Password: pass})
+    console.log(JsonObject)
+    await AsyncStorage.setItem(user, JsonObject)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const getUserAndPass = async (value) => {
+  try {
+    const userInfor = await AsyncStorage.getItem(value)
+    return userInfor != null ? JSON.parse(userInfor) : null;
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+const RegisterScreen = ({ navigation }) => {
+  const [regPass, setRegPass] = useState('');
+  const [regEmail, setRegMail] = useState('');
+  
+  const checkInforRegister = () => {
+    if (regEmail === '') {
+      console.log('email is not empty!');
+      return false;
+    }
+    if (regEmail.match(/\s/)) {
+      console.log('email is not contain any space!');
+      return false;
+    }
+    if (regPass === '') {
+      console.log('password is not empty!');
+      return false;
+    }
+    if (regPass.length < 8) {
+      console.log("password's length must bigger than 8");
+      return false;
+    }
+    return true;
+  }
+  
+  const registerSuccess = () => {
+    if (checkInforRegister()) {
+      alert('Register successful!'); 
+      storage(regEmail, regPass);
+      navigation.replace('LoginScr');
+    }
+  }
+  return (<View style={{ flex: 1 }}>
+    <Image source={{ uri: 'https://isoftglobe.com/wp-content/uploads/2021/02/react-native.png', flex: 1, height: 250 }} />
+    <View>
+      <Text style={styles.headertext}>Sign In</Text>
+    </View>
+    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity>
+        <View style={styles.socialLoginItem}>
+          <Icon name='logo-google' size={30} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.socialLoginItem}>
+          <Icon name='logo-facebook' size={30} />
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <View style={styles.socialLoginItem}>
+          <Icon name='logo-apple' size={30} />
+        </View>
+      </TouchableOpacity>
+    </View>
+    <View style={{ alignItems: 'center' }}>
+      <Text >Or register with Email...</Text>
+    </View>
+    <View>
+      <View style={styles.inputContainerStyle}>
+        <Icon style={styles.iconStyle} name='at' color='grey' size={20} />
+        <TextInput
+          placeholder="Email ID"
+          onChangeText={mail => setRegMail(mail)}
+        />
+      </View>
+      <View style={styles.inputContainerStyle}>
+        <Icon style={styles.iconStyle} name='lock-closed-outline' size={20} color='grey' />
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={pass => setRegPass(pass)}
+        />
+      </View>
+    </View>
+    <TouchableOpacity onPress={registerSuccess}>
+      <View style={styles.button}>
+        <Text >Register</Text>
+      </View>
+    </TouchableOpacity>
+  </View>);
+}
+//////////////////
+///login Screen///
+//////////////////
+
+const LoginScreen = ({ navigation }) => {
+  const [mail, setMail] = useState('');
+  const [pass, setPass] = useState('');
+
+  const loginFunction = () => {
+     getUserAndPass(mail).then(userInfor =>{
+      console.log(1);
+      if(userInfor!==null) {
+        if(pass == userInfor.Password) {
+          navigation.replace('LoginDetailScr');
+        }
+      }
+    });
+    
+    
+  };
+  const socialLoginFunc = async() => {
+    alert('Login!');
+   var user = await getUserAndPass('test');
+   console.log(user);
+  };
+  return (
+    <View style={{ flex: 1 }}>
+      <Image source={{
+        uri: 'https://isoftglobe.com/wp-content/uploads/2021/02/react-native.png', flex: 1 , height: 250,
+      }}
+      />
+      <Text style={styles.headertext}>Login</Text>
+      <View style={styles.inputContainerStyle}>
+        <Icon style={styles.iconStyle} name='at' color='grey' size={20} />
+        <TextInput
+          placeholder="Email ID"
+          onChangeText={mail => setMail(mail)}
+        />
+      </View>
+      <View style={styles.inputContainerStyle}>
+        <Icon style={styles.iconStyle} name='lock-closed-outline' size={20} color='grey' />
+        <TextInput
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={pass => setPass(pass)}
+        />
+        <Text style={{ color: 'blue', marginTop: 13, marginLeft: '60%' }}
+          onPress={() => Linking.openURL('#')}>Forgot?</Text>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={loginFunction}><Text>Login</Text></TouchableOpacity>
+
+      <View >
+        <Text style={{ textAlign: 'center', marginTop: 50 }}>Or, Login with...</Text>
+      </View>
+      <View style={styles.socialLoginContainer}>
+        <TouchableOpacity onPress={socialLoginFunc} >
+          <View style={styles.socialLoginItem} >
+            <Icon name='logo-google' size={30} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={socialLoginFunc}>
+          <View style={styles.socialLoginItem}>
+            <Icon name='logo-facebook' size={30} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={socialLoginFunc}>
+          <View style={styles.socialLoginItem}>
+            <Icon name='logo-apple' size={30} />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+        <Text>New to iThingLogistics? <Text style={{ color: 'blue' }} onPress={() => { navigation.navigate('RegisterScr') }}>Register</Text></Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headertext: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: 30
+  },
+  inputContainerStyle: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: "grey",
+    marginLeft: 10,
+    marginRight: 10
+  },
+  iconStyle: {
+    marginTop: 10,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#89CFF0',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 25,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  socialLoginContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 50
+  },
+  socialLoginItem: {
+    padding: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 10
+  }
+});
+
+
+export default App;
---
+++ loginDetail.js
@@ -0,0 +1,163 @@
+import React, { useState ,useRef} from "react";
+import {View, StyleSheet, Text,Image, FlatList, ActivityIndicator,Pressable} from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from 'react-native-vector-icons/Ionicons';
+
+var id = 0;
+
+const Home =() => {
+    const flatlistRef = useRef();
+    const loadmore = () => {
+        if(flatlistRef.current.scrollToEnd({animating: true}))
+            return <View>
+            <FlatList
+                data={ListItems}
+                renderItem={renderItem}
+                keyExtractor={item=>item.title}
+                ref={flatlistRef}
+            />
+        </View>
+      };
+    return(
+        <View>
+            <FlatList
+                data={ListItems}
+                renderItem={renderItem}
+                keyExtractor={item=>item.title}
+                ref={flatlistRef}
+            />
+        </View>
+    );
+}
+
+const Profile =() => {
+    //const [id, setId] = useState(0);
+    
+    return(
+        <View>
+            {generateListItems(id)}
+        </View>
+    );
+   
+}
+const Notifications =() => {
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>Notifications Screen</Text>
+        </View>
+    );
+}
+const Tabs = createBottomTabNavigator();
+const detailScreen = () => 
+{
+    return(
+           <Tabs.Navigator
+           screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'home'
+                  : 'home-outline';
+              } else if (route.name === 'Profile') {
+                iconName = focused ? 'person' : 'person-outline';
+              }
+              else {
+                iconName = focused ? 'notifications' : 'notifications-outline';
+              }
+  
+              // You can return any component that you like here!
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'chocolate',
+            tabBarInactiveTintColor: 'gray'
+            })} >
+               <Tabs.Screen name='Home' component={Home}  options={{headerShown:true}}/>
+               <Tabs.Screen name='Profile' component={Profile} options={{headerShown:true}} />
+               <Tabs.Screen name='Notifications' component={Notifications} options={{headerShown:true}} />
+           </Tabs.Navigator>
+    )
+}
+const renderItem = ({item}) => { return(<Item title={item.title} category={item.category} timestamp = {item.timestamp} thumb= {item.thumb} />)};
+
+const Item = ({thumb, title, category, timestamp}) => {
+    return (
+        <View style={styles.item}>
+            <Image style={{borderRadius:5}} source={{uri:thumb ,width: 60, height: 60}}/>
+            <View style={{marginLeft:15}}>
+            <Text>Title:{title}</Text>
+            <Text>Category:{category}</Text>
+            <Text>Time:{convertTime(timestamp)}</Text>
+         </View>
+        </View>
+    );
+}
+const generateListItems = (id) => {
+    var itemContent = [];
+    for(let i = id; i < 10 ; i++) {
+        if(i&1)
+        itemContent.push(<ItemDetail title={i} thumb={ListItems[0].thumb} cat={ListItems[0].category} timestamp={ListItems[0].timestamp}></ItemDetail>);
+        else
+        itemContent.push(<ItemDetail title={i} thumb={ListItems[1].thumb} cat={ListItems[1].category} timestamp={ListItems[1].timestamp}></ItemDetail>);
+    }
+    id+=10;
+    return itemContent;
+}
+const convertTime = (timestamp) => {
+    var years, months, days, hours, minutes, seconds;
+    var currentSeconds = Date.now()/1000;
+    seconds = (currentSeconds - timestamp);
+    minutes = Math.floor(seconds/60);
+    hours = Math.floor(minutes/60);
+    days = Math.floor(hours/24);
+    months = Math.floor(days/30);
+    years = Math.floor(months/12);
+ 
+    if(years > 0) return years == 1 ? 'a year ago' : years + ' years ago';
+    if(months > 0) return months == 1 ? 'a month ago' : months + ' months ago'; 
+    if(days > 0) return days == 1 ? 'a day ago' : days+ ' days ago';
+    if(hours > 0) return hours == 1 ? hours + 'an hour ago' : hours + ' hours ago';
+    if(minutes > 0) return minutes == 1 ?'a minute ago' : minutes + ' minutes ago'; 
+    if(seconds > 0) return seconds == 1 ?'a second ago' : seconds + ' seconds ago'; 
+    
+ }
+
+const ItemDetail = ({thumb, title, cat, timestamp}) => {
+   return (
+     <View style={styles.item}>
+         <Image style={{borderRadius:5}} source={{uri:thumb ,width: 60, height: 60}}/>
+         <View style={{marginLeft:15}}>
+            <Text>Title:{title}</Text>
+            <Text>Category:{cat}</Text>
+            <Text>Time:{convertTime(timestamp)}</Text>
+         </View>
+     </View>
+   )
+ }
+
+ const ListItems=[
+   {title:1, category:'cat', thumb:'https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Night%20Sky%20WB.jpeg?itok=jxh1nTJA%27',timestamp:'1643256662'},
+   {title:2, category:'cat', thumb:'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80',timestamp:'1643256662'},
+   {title:3, category:'cat', thumb:'https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Night%20Sky%20WB.jpeg?itok=jxh1nTJA%27',timestamp:'1643256662'},
+   {title:4, category:'cat', thumb:'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80',timestamp:'1643256662'},
+   {title:5, category:'cat', thumb:'https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Night%20Sky%20WB.jpeg?itok=jxh1nTJA%27',timestamp:'1643256662'},
+   {title:6, category:'cat', thumb:'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80',timestamp:'1643256662'},
+   {title:7, category:'cat', thumb:'https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Night%20Sky%20WB.jpeg?itok=jxh1nTJA%27',timestamp:'1643256662'},
+   {title:8, category:'cat', thumb:'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80',timestamp:'1643256662'},
+   {title:9, category:'cat', thumb:'https://www.sierraclub.org/sites/www.sierraclub.org/files/styles/flexslider_full/public/sierra/articles/big/SIERRA%20Night%20Sky%20WB.jpeg?itok=jxh1nTJA%27',timestamp:'1643256662'},
+   {title:10, category:'cat', thumb:'https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=759&q=80',timestamp:'1643256662'}
+ ]
+const Loadmore = () => {
+    const [isLoad, setIsLoad] = useState(true);
+    if(isLoad ===true)
+    setTimeout(setIsLoad(false),1000);
+    return <ActivityIndicator hidesWhenStopped={isLoad}/>;
+}
+const styles = StyleSheet.create({
+    item: {
+        flexDirection:'row',
+        margin:10
+    }
+    
+});
+export default detailScreen;
